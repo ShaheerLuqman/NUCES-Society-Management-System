c2b5d39670bbfff4a4e289f1e271a72c2427f11d
ShaheerLuqman <94440745+ShaheerLuqman@users.noreply.github.com>
2022-03-23 01:19:33 +0500

    Add files via upload

---
+++ login.h
@@ -0,0 +1,90 @@
+/*
+    ● Option for accounts registered to remain saved
+    ● New Questions for account register
+    ● Copy data to local variables when Account logs in
+    ● https://www.geeksforgeeks.org/readwrite-class-objects-fromto-file-c/
+*/
+
+using namespace std;
+int login()
+{
+    int choice;
+    string name, password, inName, inPassword, registerName, registerPassword;
+
+    while (1)
+    {
+        cout << "Press\n"
+             << "   1. Register\n"
+             << "   2. Login\n"
+             << "   0. Exit\n"
+             << "Your Input: ";
+        cin >> choice;
+
+        if (choice == 0)
+        {
+            exit(EXIT_SUCCESS);
+        }
+        else if (choice == 1) // Register
+        {
+            ofstream g("registration.txt"); /* ofstream is the one for getting data from the file, and the file does not even have to exist. If it's ofstream, it'll take care of it for you. but be warned that if there is a file called "registration.txt" in the name folder as the .exe file, the contents will be deleted*/
+            if (!g.is_open())               // if it's not open, then there is no such file with the given name inside
+            {
+                cout << "could not open file\n";
+                exit(EXIT_FAILURE);
+            }
+
+            cout << "\n********** Account Register **********\n"
+                 << "New Username: ";
+            fflush(stdin);
+            getline(cin, registerName);
+            cout << "New Password: ";
+            fflush(stdin);
+            getline(cin, registerPassword);
+            g << registerName;
+
+            g << '\n';
+            g << registerPassword;
+
+            g.close();
+        }
+        else if (choice == 2) // Login
+        {
+            ifstream f("registration.txt"); // ifstream is the one for getting data from the file, and
+            // let us assume you've already created a file called "registration.txt"
+            if (!f.is_open()) // if it's not open, then there is no such file with the given name inside
+            // the folder (that is, in the folder where the .exe file is going to be)
+            {
+                cout << "could not open file\n"; // just so that you know why it won't work if it doesn't
+                exit(EXIT_FAILURE);
+            }
+
+            getline(f, name, '\n');     // reads the user name from file f (which is using "registration.txt")
+            getline(f, password, '\n'); // reads the password from file f (which is using "registration.txt")
+            // also, if you tell the file to get you that text up until '\n', that's when you know it reads
+            // the whole line at most, and won't go any further
+            // and that is done by the 3rd parameter
+            f.close();
+
+            while (1)
+            {
+                cout << "\n*********** Account Login ***********\n"
+                     << "Enter Username: ";
+                fflush(stdin);
+                getline(cin, inName);
+                cout << "Enter Password: ";
+                fflush(stdin);
+                getline(cin, inPassword);
+                if (inName == name && inPassword == password)
+                {
+                    cout << "Login Successful\n"
+                         << "Welcome, "
+                         << inName;
+                    return 1;
+                    break;
+                }
+                cout << "incorrect name or password\n";
+            }
+        }
+        cout << "\n######################################\n\n";
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,40 +1,47 @@
 /*
-- Following OOP concepts are expected to be part of the project:
-    - Abstraction
-    - Classes and Objects
-    - Encapsulations
-    - Inheritances
-    - Polymorphism (Both overloading and overriding)
-    - Conditional and looping structures
-    - Data structure such as arrays, array of objects, arraylists, etc.
-    - Constructors and destructors
-    - Filing and data storage systems
-    - Exception handling
-*/
-
-/*
 OBJECTIVES:
 Partially Done:
     ● Account registry
-    ● Allocation of accessibility to different accounts (example, Personal accounts can be updated
-    by the people personally, Society head can update info regarding their society, etc.)
     ● Information Regarding Events (Past, Ongoing and Future)
 
 To be done later:
+    ● Allocation of accessibility to different accounts (example, Personal accounts can be updated
+        by the people personally, Society head can update info regarding their society, etc.)
     ● Calendar (to display important dates)
     ● Notifications (to notify users about upcoming events and deadlines.)
     ● Registration process for students to apply for society’s membership.
     ● Evaluation of the applicants on the basis of information provided by applicants (It will help
-    faculty/Society managers in considering who should be the best candidate)
+        faculty/Society managers in considering who should be the best candidate)
 */
 
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <fstream>
+#include "login.h"
 using namespace std;
 
-void SignUp(){}; // Input will be taken, then input will be validated and verified, then respective account object will be created
-void SignIn(){};
+void menu()
+{
+    int choice;
+    system("cls");
+    cout << "Welcome to Nuces Society Management System";
+    cout << "\nPress\n"
+         << "   1. Log In or Register\n"
+         // << "   2. Login\n"
+         << "   0. Exit\n"
+         << "Your Input: ";
+    cin >> choice;
+    if (choice == 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else if (choice == 1)
+    {
+        system("cls");
+        login();
+    }
+};
 
 class date
 {
@@ -79,12 +86,21 @@ private:
     string password;
     string type;              // e.g student, faculty, head, society head, director, visitor
     bool Modification_rights; // head, society head, director can apply
+    string id;
 
 public:
     void setName(string temp) { Full_Name = temp; }
     void setAge(int temp) { Age = temp; }
     string getName() { return Full_Name; }
     int getAge() { return Age; }
+};
+
+class Member : public Account
+{
+private:
+    string Member_Role;
+
+public:
 };
 
 class Events // Inherited classes will be past events and future events
@@ -100,7 +116,34 @@ private:
 public: // Functions are to be made
 };
 
+class Society
+{
+private:
+    string Society_Name;
+    string Society_Description;
+    int Number_Of_Events;
+    Events events[1];  // value to be modified according to number of events
+    Member members[1]; // value to be modified according to number of members
+
+public:
+};
+
 int main()
 {
+    menu();
     return 0;
 }
+
+/*
+- Following OOP concepts are expected to be part of the project:
+    - Abstraction
+    - Classes and Objects
+    - Encapsulations
+    - Inheritances
+    - Polymorphism (Both overloading and overriding)
+    - Conditional and looping structures
+    - Data structure such as arrays, array of objects, arraylists, etc.
+    - Constructors and destructors
+    - Filing and data storage systems
+    - Exception handling
+*/
