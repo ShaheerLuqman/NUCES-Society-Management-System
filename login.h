c091216db0c261b257658c00a8f093c834232cea
ShaheerLuqman <94440745+ShaheerLuqman@users.noreply.github.com>
2022-03-25 21:36:57 +0500

    Add files via upload

--- login.h
+++ login.h
@@ -8,8 +8,10 @@
 using namespace std;
 int login()
 {
-    int choice;
-    string name, password, inName, inPassword, registerName, registerPassword;
+    int choice, status, contact_number, faculty_year;
+    float cgpa;
+    string name, password, inName, inPassword, registerName, registerPassword, registerRePassword,
+        std_id, email, gender, alumni_academic, occupation, department, job, alumni_graduation, teacher_id;
 
     while (1)
     {
@@ -39,15 +41,85 @@ int login()
                  << "New Username: ";
             fflush(stdin);
             getline(cin, registerName);
+            cout << "E-mail Address: ";
+            fflush(stdin);
+            getline(cin, email);
+            cout << "Contact Number: ";
+            cin >> contact_number; // have to check data type of number.
+            cout << "Gender: ";
+            fflush(stdin);
+            getline(cin, gender);
+            //            cout<<"Society: ";
+            //            getline(cin,society);
+            cout << "Status: " << endl;
+            cout << "Press" << endl
+                 << "1.Student\n" // have to pass control to desired choice
+                 << "2.Faculty member\n"
+                 << "3.Alumni\n";
+
+            cin >> status;
+            if (status == 1)
+            {
+                cout << "Student ID (e.g: 21K-XXXX): ";
+                fflush(stdin);
+                getline(cin, std_id);
+                cout << "CGPA: ";
+                cin >> cgpa; // do we have to impose restriction on below 2 cgpa?
+                cout << "Department: ";
+                fflush(stdin);
+                getline(cin, department);
+            }
+            else if (status == 2)
+            {
+                cout << "Teacher ID (e.g: 21K-XXXX): ";
+                fflush(stdin);
+                getline(cin, teacher_id);
+                cout << "Occupation: "; // occupation or designation
+                fflush(stdin);
+                getline(cin, occupation);
+                cout << "Workin since: (year)";
+                cin >> faculty_year;
+                cout << "Department: ";
+                fflush(stdin);
+                getline(cin, department);
+            }
+            else if (status == 3)
+            {
+                cout << "Student ID (e.g: 21K-XXXX): ";
+                fflush(stdin);
+                getline(cin, std_id);
+                cout << "CGPA: ";
+                cin >> cgpa; // do we have to impose restriction on below 2 cgpa?
+                cout << "Graduation department: ";
+                fflush(stdin);
+                getline(cin, alumni_graduation);
+                cout << "Current Academic program (if enrolled in any): ";
+                fflush(stdin);
+                getline(cin, alumni_academic);
+                cout << "Current job: (if any)";
+                fflush(stdin);
+                getline(cin, job);
+            }
             cout << "New Password: ";
             fflush(stdin);
             getline(cin, registerPassword);
-            g << registerName;
+            cout << "Re-enter Password: ";
+            fflush(stdin);
+            getline(cin, registerRePassword);
+            if (registerPassword == registerRePassword)
+            {
+                g << registerName;
 
-            g << '\n';
-            g << registerPassword;
+                g << '\n';
+                g << registerPassword;
 
-            g.close();
+                g.close();
+            }
+            else
+            {
+                cout << "Password not matched.";
+                // control back to re-enter password.
+            }
         }
         else if (choice == 2) // Login
         {
