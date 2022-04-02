cc05faa201a820abb68b69b4b8e6adc675dd7bf0
ShaheerLuqman <94440745+ShaheerLuqman@users.noreply.github.com>
2022-04-02 14:30:27 +0500

    Add files via upload

--- register&login.h
+++ register&login.h
@@ -10,18 +10,16 @@ using namespace std;
 class Account
 {
 private:
-    string Full_Name, username, password, status, email, gender; // e.g student, faculty, head, society head, director, visitor
-    bool Modification_rights;                                    // head, society head, director can apply
+    string Full_Name, username, password, status, email, gender, contact_number; // e.g student, faculty, head, society head, director, visitor
+    bool Modification_rights;                                                    // head, society head, director can apply
     int Age;
-    long long int contact_number;
-
     float cgpa;
-    string designation, std_id, alumni_academic, occupation, department, job, alumni_graduation, faculty_id;
 
 public:
     void setName(string temp) { Full_Name = temp; }
     void setAge(int temp) { Age = temp; }
-    string getName() { return Full_Name; }
+    string getUserName() { return username; }
+    string getPassword() { return password; }
     int getAge() { return Age; }
     void Account_register()
     {
@@ -30,20 +28,23 @@ public:
              << "New Username: ";
         fflush(stdin);
         getline(cin, username);
-        cout << "New Password: ";
-        fflush(stdin);
-        getline(cin, password);
-        string registerRePassword;
-        cout << "Re-enter Password: ";
-        fflush(stdin);
-        getline(cin, registerRePassword);
-        if (password == registerRePassword)
+        while (1)
         {
-            cout << "Values Inserted Successfuly" << endl;
-        }
-        else
-        {
-            cout << "Password not matched." << endl;
+            cout << "New Password: ";
+            fflush(stdin);
+            getline(cin, password);
+            string registerRePassword;
+            cout << "Re-enter Password: ";
+            fflush(stdin);
+            getline(cin, registerRePassword);
+            if (password == registerRePassword)
+            {
+                break;
+            }
+            else
+            {
+                cout << "Password not matched." << endl;
+            }
         }
         cout << "E-mail Address: ";
         fflush(stdin);
@@ -195,11 +196,10 @@ int login()
                 exit(EXIT_FAILURE);
             }
 
-            cout << "Status: " << endl;
-            cout << "Enter your number" << endl
-                 << "1.Student\n"
-                 << "2.Faculty\n"
-                 << "3.Alumni\n";
+            cout << "Enter number which describes you! " << endl
+                 << "1. Student\n"
+                 << "2. Faculty\n"
+                 << "3. Alumni\n";
             cin >> choice;
             if (choice == 1)
             {
@@ -207,11 +207,7 @@ int login()
                 Student std;
                 std.Account_register();
                 g.write((char *)&std, sizeof(std));
-
-                ifstream f("registration.txt", ios::in);
-                Student std1;
-                f.read((char *)&std1, sizeof(std1));
-                std1.DisplayAccountData();
+                g.close();
             }
             else if (choice == 2)
             {
@@ -224,18 +220,22 @@ int login()
         }
         else if (choice == 2) // Login
         {
-            ifstream f("registration.txt");
+            ifstream f("registration.txt", ios::in);
             if (!f.is_open())
             {
                 cout << "could not open file\n";
                 exit(EXIT_FAILURE);
             }
 
-            getline(f, name, '\n');     // reads the user name from file f (which is using "registration.txt")
-            getline(f, password, '\n'); // reads the password from file f (which is using "registration.txt")
-            // also, if you tell the file to get you that text up until '\n', that's when you know it reads
-            // the whole line at most, and won't go any further
-            // and that is done by the 3rd parameter
+            Student std1;
+            f.read((char *)&std1, sizeof(std1));
+            std1.DisplayAccountData();
+
+            // getline(f, name, '\n');     // reads the user name from file f (which is using "registration.txt")
+            // getline(f, password, '\n'); // reads the password from file f (which is using "registration.txt")
+            // // also, if you tell the file to get you that text up until '\n', that's when you know it reads
+            // // the whole line at most, and won't go any further
+            // // and that is done by the 3rd parameter
             f.close();
 
             while (1)
@@ -247,11 +247,12 @@ int login()
                 cout << "Enter Password: ";
                 fflush(stdin);
                 getline(cin, inPassword);
-                if (inName == name && inPassword == password)
+                if (inName == std1.getUserName() && inPassword == std1.getPassword())
                 {
                     cout << "Login Successful\n"
                          << "Welcome, "
                          << inName;
+                    std1.DisplayAccountData();
                     return 1;
                     break;
                 }
